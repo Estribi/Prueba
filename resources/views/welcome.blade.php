95131af8b1ce39fc3405e017a95f92f388d75ec4
José Estribí <jose26.e.r@gmail.com>
2020-02-23 14:52:31 -0500

    Auto stash before merge of "master" and "origin/master"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,8 +93,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                   
-                <h1>Prueba2</h1>
+
                 </div>
             </div>
         </div>
