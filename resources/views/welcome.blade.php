e909c7171fb3f5d9fab195ea2557b25077ca437e
José Estribí <jose26.e.r@gmail.com>
2020-02-23 15:46:34 -0500

    P1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,7 +93,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-
+<h1>Modificaciones del P1</h1>
                    
                 </div>
             </div>
