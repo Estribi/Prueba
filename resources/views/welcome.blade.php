c9459f28c96911be6db9384a1597516f901b6245
José Estribí <jose26.e.r@gmail.com>
2020-02-23 13:39:05 -0500

    Prueba Base

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -93,9 +93,7 @@
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
                     <a href="https://github.com/laravel/laravel">GitHub</a>
-                    <h1>Testing</h1>
-                    <h1>Testing 2</h1>
-                    <h1>Testing 3</h1>
+
                 </div>
             </div>
         </div>
